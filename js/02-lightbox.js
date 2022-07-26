307a5b2f9e2a159f47c8db352c12d25cef14b223
Mykola Kovalchuk <kolyankov@gmail.com>
2022-07-26 15:58:03 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,13 +6,22 @@ const imagesMarkup = createMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML("afterbegin", imagesMarkup);
 
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
 
 function createMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
+      <a class="gallery__link" href="${original}" target="_blank" rel="noopener noreferrer">
         <img
           class="gallery__image"
           src="${preview}"

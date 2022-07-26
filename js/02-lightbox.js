6617f18cbc178b626c4010b77b90f7721d43cffd
Mykola Kovalchuk <kolyankov@gmail.com>
2022-07-26 15:36:05 +0300

    insert a markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", imagesMarkup);
+
 console.log(galleryItems);
 
 function createMarkup(galleryItems) {
